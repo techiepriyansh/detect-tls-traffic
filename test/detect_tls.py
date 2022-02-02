6b4bfd72edb557ecb1daa8ec0b2487714933b7bf
Priyansh Rathi <techiepriyansh@gmail.com>
2022-02-02 23:49:34 +0530

    test sequentialiy of SSL read/writes with tcp read/write

--- test/detect_tls.py
+++ test/detect_tls.py
@@ -5,6 +5,10 @@ from bcc.utils import printb
 
 prog = """
 #include <uapi/linux/ptrace.h>
+#include <linux/tcp.h>
+#include <net/sock.h>
+#include <bcc/proto.h>
+
 
 int hook_to_SSL_CTX_new(struct pt_regs *ctx) {
     bpf_trace_printk("New SSL Context\\n");
@@ -22,8 +26,18 @@ int hook_to_SSL_do_handshake(struct pt_regs *ctx) {
     return 0;
 }
 
-int hook_to_SSL_read(struct pt_regs *ctx) {
+int hookret_to_SSL_do_handshake(struct pt_regs *ctx) {
+    bpf_trace_printk("Handshake Return\\n");
+    return 0;
+}
+
+int hook_to_SSL_read(struct pt_regs *ctx, void * ssl, void *buf, int num) {
     bpf_trace_printk("Read\\n");
+    return 0;
+}
+
+int hookret_to_SSL_read(struct pt_regs *ctx) {
+    bpf_trace_printk("Read Return %d\\n", PT_REGS_RC(ctx));
     return 0;
 }
 
@@ -32,19 +46,65 @@ int hook_to_SSL_read_ex(struct pt_regs *ctx) {
     return 0;
 }
 
-int kretprobe__tcp_v4_connect(struct pt_regs *ctx) {
-    bpf_trace_printk("TCP Connect");
+int hook_to_SSL_write(struct pt_regs *ctx) {
+    bpf_trace_printk("Write\\n");
     return 0;
 }
 
-/*int kretprobe__tcp_v4_do_rcv(struct pt_regs *ctx) {
-    u32 pid = bpf_get_current_pid_tgid()
-    bpf_trace_printk("TCP Receive");
+int hookret_to_SSL_write(struct pt_regs *ctx) {
+    bpf_trace_printk("Write Return\\n");
     return 0;
-}*/
+}
+
+int hook_to_SSL_write_ex(struct pt_regs *ctx) {
+    bpf_trace_printk("Write (ex)\\n");
+    return 0;
+}
+
+int kretprobe__tcp_v4_connect(struct pt_regs *ctx) {
+    bpf_trace_printk("TCP Connect\\n");
+    return 0;
+}
+
+int kprobe__tcp_sendmsg(struct pt_regs *ctx, struct sock *sk,
+    struct msghdr *msg, size_t size)
+{
+    u16 lport = sk->__sk_common.skc_num;
+    u16 dport = sk->__sk_common.skc_dport;
+    dport = ntohs(dport);
+
+    if (dport == 443) {
+        bpf_trace_printk("  Sent to server\\n");
+    }
+
+    return 0;
+}
+
+int kprobe__tcp_cleanup_rbuf(struct pt_regs *ctx, struct sock *sk, int copied)
+{
+    u16 lport = sk->__sk_common.skc_num;
+    u16 dport = sk->__sk_common.skc_dport;
+    dport = ntohs(dport);
+
+    if (dport == 443) {
+        bpf_trace_printk("  Recvd from server\\n");
+    }
+
+    return 0;   
+}
 """
 
-libfns = ["SSL_CTX_new", "SSL_set_fd", "SSL_do_handshake", "SSL_get_verify_result", "SSL_read", "SSL_read_ex", "SSL_accept"]
+libfns = [
+    "SSL_CTX_new", 
+    "SSL_set_fd", 
+    "SSL_do_handshake", 
+    "SSL_get_verify_result", 
+    "SSL_read", 
+    "SSL_read_ex",
+    "SSL_write",
+    "SSL_write_ex",
+    "SSL_accept",
+]
 
 '''
 Simply detecting these functions calls would only ascertain that these functions were called.
@@ -59,10 +119,14 @@ b.attach_uprobe(name="ssl", sym=libfns[1], fn_name="hook_to_SSL_set_fd")
 b.attach_uprobe(name="ssl", sym=libfns[2], fn_name="hook_to_SSL_do_handshake")
 b.attach_uprobe(name="ssl", sym=libfns[4], fn_name="hook_to_SSL_read")
 b.attach_uprobe(name="ssl", sym=libfns[5], fn_name="hook_to_SSL_read_ex")
+b.attach_uprobe(name="ssl", sym=libfns[6], fn_name="hook_to_SSL_write")
+b.attach_uprobe(name="ssl", sym=libfns[7], fn_name="hook_to_SSL_write_ex")
 
+b.attach_uretprobe(name="ssl", sym=libfns[2], fn_name="hookret_to_SSL_do_handshake");
+b.attach_uretprobe(name="ssl", sym=libfns[4], fn_name="hookret_to_SSL_read")
+b.attach_uretprobe(name="ssl", sym=libfns[6], fn_name="hookret_to_SSL_write")
 
-print("Starting to trace...")
-
+print("Starting to trace...", flush=True)
 while 1:
     try:
         (task, pid, cpu, flags, ts, msg) = b.trace_fields()
